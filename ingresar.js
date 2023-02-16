186ed4cb91900198a543f16c84436c6d600a9772
yury1031 <yuryrueda2013@gmail.com>
2023-02-16 08:24:32 -0500

    version inicial

---
+++ ingresar.js
@@ -0,0 +1,27 @@
+
+
+const usuario = document.getElementById("usuario")
+const pass = document.getElementById("pass")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    parrafo.innerHTML = ""
+    if(usuario.value.length <4){
+        warnings += `El usuario no es valido <br>`
+        entrar = true
+    }
+    if(pass.value.length < 8){
+        warnings += `La contraseña no es valida <br>`
+        entrar = true
+    }
+
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }else{
+        parrafo.innerHTML = "Enviado"
+    }
+})
